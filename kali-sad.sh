fb6088ebb572a1fced5365dfe9ce98abb35c8f30
REN9999 <sejatipria990@gmail.com>
2024-07-26 13:01:32 +0700

    Create kali-sad.sh

---
+++ kali-sad.sh
@@ -0,0 +1,23 @@
+echo
+sleep 1.5
+echo "kalo udah install kali Linux ini jangan di install lagi yaa tod ❗❗❗"
+sleep 1
+read -p"𝐩𝐢𝐥𝐢𝐡 𝐲/𝐧  " p
+if [ $p == y ]
+then
+sleep 2
+clear
+echo"installing......"
+sleep 1
+pkg update && pkg upgrade
+pkg install wget proot
+wget -O install-kali.sh https://raw.githubusercontent.com/EXALAB/Anlinux-Resources/maste>
+chmod +x install-kali.sh
+./install-kali.sh
+./start-kali.sh
+fi
+if [ $p == n ]
+then
+sleep 2
+exit
+fi
